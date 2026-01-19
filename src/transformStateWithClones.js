1f3ddc58638e3b6170585d7cb74e2b5810bf32fa
Serhii Shimko <shimkosergij2010@gmail.com>
2026-01-19 11:59:37 +0200

    completed the task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const cloneState = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const remove of action.keysToRemove) {
+        delete currentState[remove];
+      }
+    }
+
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    cloneState.push(currentState);
+  }
+
+  return cloneState;
 }
 
 module.exports = transformStateWithClones;
